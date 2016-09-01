02c91c2aab3b6b6b0e38432009aaed915dcec071
Estu Fardani <andro.medh4@gmail.com>
2016-09-01 08:13:04 +0700

    [Estu] add support for send attach file to all rcptname

--- simple-mailer.py
+++ simple-mailer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+# please setup sender account
+# please file receipe meet you need
+
 import time
 import smtplib
 import csv
@@ -14,6 +17,7 @@ csvFile = csv.reader(data)
 mailfrom = "Saya <my@domain.com>"
 for row in csvFile:
     rcptname = str(row[0]) + " " + str(row[1])
+    lampiran="/path/to/attch2/FileNameSameAsRcptName-"+str(row[0]) + str(row[1]) + ".pdf"
     mailto = str(row[2])
     contentFile = open('mymessage.txt', 'r')
     msg = MIMEMultipart()
@@ -22,6 +26,13 @@ for row in csvFile:
     msg['To'] = rcptname + " <" + mailto + ">"
     msg['Subject'] = "Subyek Email"
     msg.attach(MIMEText( "Dear "+ rcptname + ",\n\n" + contentFile.read() ))
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open(lampiran, "rb").read())
+    Encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment', filename="FileNameSameAsRcptName-"+str(row[0]) + str(row[1]) + ".pdf")
+    msg.attach(part)
+
     server = smtplib.SMTP('mail.domain.com', 587)
     server.starttls()
     server.ehlo()
